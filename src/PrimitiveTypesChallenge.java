4fd912875f69f69f499b80c6542889aab3fca9a5
jasper <whiteana001@warren.k12.in.us>
2024-08-05 13:31:13 -0400

    1.3.1

--- src/PrimitiveTypesChallenge.java
+++ src/PrimitiveTypesChallenge.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PrimitiveTypesChallenge {
+public class PrimitiveTypesChallenge {
+    public static void main(String[] args) {
+        byte myByte = 30;
+        short myShort = 9;
+        int myInt = 200;
+        long myLong = 80000 - 5 * (myInt  + myByte + myShort);
+
+        System.out.println(myLong);
+    }
 }
